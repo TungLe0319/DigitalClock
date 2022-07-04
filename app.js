64589f7315c8d217ce2cc5cbf6a9d00627308fca
tungle0319 <107594367+tungle0319@users.noreply.github.com>
2022-07-04 09:08:45 -0600

    digital Clock

---
+++ app.js
@@ -0,0 +1,21 @@
+function clock() {
+  var fullDate = new Date();
+  var hours = fullDate.getHours();
+  var mins = fullDate.getMinutes();
+  var seconds = fullDate.getSeconds();
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  if (mins < 10) {
+    mins = '0' + mins;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+
+  document.getElementById('hour').innerHTML = hours;
+  document.getElementById('minute').innerHTML = ':' + mins;
+  document.getElementById('seconds').innerHTML = ':' + seconds;
+}
+
+setInterval(clock, 100);
